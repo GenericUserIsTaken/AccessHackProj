de7e8de3fb870bcf50e2d38a4995a18efee09c48
Reese G <86173015+valsei@users.noreply.github.com>
2025-02-16 15:53:16 -0800

    changes to global lmao
    mergeconflicter no conflicting

--- checklist.gd
+++ checklist.gd
@@ -5,36 +5,48 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	# if the json is loaded already, immediately load the activities list
+	# otherwise, wait for the loaded signal from the Global script
+	if Global.has_loaded_activities_json:
+		rebuild_activities_list()
+	else:
+		Global.loaded_activities_json.connect(_on_loaded_activities_json)
+
+func _on_loaded_activities_json():
+	rebuild_activities_list()
+
+func rebuild_activities_list():
+	# build a tree from the json file of activities
 	a_list.create_item()
 	
-	var json = JSON.new()
-	var activities = JSON.parse_string(FileAccess.get_file_as_string("res://data/activities.json"))
-	
-	for activity_name in activities:
+	for activity_name in Global.activities:
 		var activity = a_list.create_item()
 		activity.set_text(0, activity_name.capitalize())
-		#activity.collapsed = true
 		
-		for catagory_name in activities[activity_name]:
+		for catagory_name in Global.activities[activity_name]:
 			var catagory = a_list.create_item(activity)
 			catagory.set_cell_mode(0, TreeItem.CELL_MODE_CHECK)
 			catagory.set_editable(0, true)
 			catagory.set_text(0, catagory_name.capitalize())
-			catagory.set_metadata(0, catagory_name)
+			catagory.set_metadata(0, activity_name + "," + catagory_name)
 
 func _on_activities_list_item_edited() -> void:
-	record_checked_catagories()
-
-func record_checked_catagories():
 	
-	var checked_list = []
+	# rebuild a list of all chosen activities
+	Global.chosen_activities.clear()
 	
-	var tree_node = $"%ActivitiesList".get_root().get_first_child()
+	var tree_node = a_list.get_root().get_first_child()
 	while tree_node != null:
 		
 		if tree_node.is_checked(0):
-			checked_list.append(tree_node.get_metadata(0))
+			var activity_as_string = tree_node.get_metadata(0)
+			Global.chosen_activities.append(activity_as_string)
 		
 		tree_node = tree_node.get_next_in_tree()
 	
-	print(checked_list)
+	print(Global.chosen_activities)
+
+func rebuild_chosen_list():
+	c_list.clear()
+	c_list.create_item()
--- global.gd
+++ global.gd
@@ -1,12 +1,19 @@
 extends Node
 
+signal loaded_activities_json()
+var has_loaded_activities_json := false
+
+var activities = {}
+var chosen_activities = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("hello")
-	pass # Replace with function body.
+	load_activities_json()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func load_activities_json():
+	var json = JSON.new()
+	activities = JSON.parse_string(FileAccess.get_file_as_string("res://data/activities.json"))
+	has_loaded_activities_json = true
+	loaded_activities_json.emit()
+	print("loaded activities json")

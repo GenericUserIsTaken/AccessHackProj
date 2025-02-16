ee50b8bf994aee5bea26773385614369095ea37f
IsaacZ <152723139+IsaacZappone@users.noreply.github.com>
2025-02-16 12:26:35 -0800

    generateLines (types dont change color yet though)

---
+++ generateLines.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func generateLines(roadArray):
+	var line2Ds = []
+	for i in range(roadArray.size()):
+		line2Ds.append(Line2D.new())
+		
+		#NOT FINISHED, need to figure out how color 
+		#relates to type
+		#line2Ds[i].color = Color("red")
+		
+		for j in range(roadArray[i].positions.size()):
+			line2Ds[i].add_point(roadArray[i].positions[j], line2Ds[i].get_point_count())
+	

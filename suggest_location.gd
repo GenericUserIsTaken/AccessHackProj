4801a5796369e28ae6ca1ada5021b65dda1bd0af
Ozknight <76978960+Ozknightfasefgadadfddsafdaasda@users.noreply.github.com>
2025-02-18 23:27:24 -0800

    Suggestions WORK LETS GO BBAY
    SKIB VAR SKIBIDY

--- suggest_location.gd
+++ suggest_location.gd
@@ -1,11 +1,30 @@
 extends Control
+@onready var LocationName = $MarginContainer/VBoxContainer/HBoxContainer2/LineEdit
+@onready var Address = $MarginContainer/VBoxContainer/HBoxContainer3/LineEdit
+@onready var Link = $MarginContainer/VBoxContainer/HBoxContainer5/LineEdit
+@onready var hours = $MarginContainer/VBoxContainer/HBoxContainer4/LineEdit
+@onready var Description = $MarginContainer/VBoxContainer/HBoxContainer6/TextEdit
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
+@onready var Entries = [LocationName, Address, Link, hours, Description]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#Called when Button IS PRESSED DUH 
+func _on_button_pressed() -> void:
+	for i in Entries.size():
+		if(Entries[i].text.is_empty()) : 
+			print("empty")
+			return
+	
+	var ActivityData = {}
+	ActivityData["address"] = Address.text
+	ActivityData["link"] = Link.text
+	ActivityData["hours"] = hours.text
+	ActivityData["description"] = Description.text
+	
+	Global.activities ["user_submitted"]["suggested_locations"][LocationName.text] = ActivityData
+	Global.rebuild_scheduler.emit()
+	
+	for j in Entries.size():
+		Entries[j].text = ""
+	
